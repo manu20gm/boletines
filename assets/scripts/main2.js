3ec56f9e050492399b3621b2066ba7fcf5585a16
Manuel Ángel Guerrero Murillo <manuel.guerrero@universitasxxi.com>
2021-12-28 11:41:04 +0100

    Arreglo de ruta /

--- assets/scripts/main2.js
+++ assets/scripts/main2.js
@@ -15,7 +15,7 @@ links.forEach(function(link){
 		content.classList.add("animate__animated");
 		
 		setTimeout(function(){
-			location.href="/boletines";
+			location.href="/";
 		},600);
 		
 		return false;
